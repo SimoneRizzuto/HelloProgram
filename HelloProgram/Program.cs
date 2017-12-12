21397666fa2f95fecea7ebbcad0e5d5b0722964b
Simorico <koala.simone@gmail.com>
2017-12-12 11:12:36 +1000

    Initial Commit

---
+++ HelloProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
